80f8b92027495b81099fb814c59f8be1c9762821
Jia <jiafeng3124@gmail.com>
2019-04-10 20:17:23 -0700

    add overview and reviews modules to proxy server

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
+
+const overview = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+const reservations = axios.create({
+  baseURL: 'http://localhost:3002',
+});
+
+const menu = axios.create({
+  baseURL: 'http://localhost:3003',
+});
+
+const reviews = axios.create({
+  baseURL: 'http://localhost:3004',
+});
+
+app.use('/api/restaurant/:rid', (req, res) => {
+  overview.get(`/api/restaurant/${req.params.id}`)
+    .then(response => res.json(response.data))
+    .catch(err => res.status(400).send(err));
+})
+
+
+app.use('/api/restaurants/:id/reviews', (req, res) => {
+  reviews.get(`/api/restaurants/${req.params.id}/reviews`)
+    .then(response => res.json(response.data))
+    .catch(err => res.status(400).send(err));
+})
+
+app.use('/api/restaurants/:id/filters', (req, res) => {
+  reviews.get(`/api/restaurants/${req.params.id}/filters`)
+    .then(response => res.json(response.data))
+    .catch(err => res.status(400).send(err));
+})
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
