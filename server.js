807ee698a22ec7965df409d4cec21cfb0173026f
Jay Tong <jayktong@gmail.com>
2019-05-03 09:04:54 -0700

    Update port and api endpoint for menu module

--- server.js
+++ server.js
@@ -3,33 +3,32 @@ const morgan = require('morgan');
 const path = require('path');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 80;
 
-app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/restaurants/:id', express.static(path.join(__dirname, 'public')));
 
-const overview = axios.create({
-  baseURL: 'http://localhost:3001',
-});
+// const overview = axios.create({
+//   baseURL: 'http://localhost:3001',
+// });
 
 // const reservations = axios.create({
 //   baseURL: 'http://localhost:3002',
 // });
 
 const menu = axios.create({
-  baseURL: 'http://localhost:3003',
+  baseURL: 'http://ec2-54-183-59-210.us-west-1.compute.amazonaws.com',
 });
 
-const reviews = axios.create({
-  baseURL: 'http://localhost:3004',
-});
+// const reviews = axios.create({
+//   baseURL: 'http://localhost:3004',
+// });
 
-app.use('/api/restaurant/:rid', (req, res) => {
-  overview.get(`/api/restaurant/${req.params.id}`)
-    .then(response => res.json(response.data))
-    .catch(err => res.status(400).send(err));
-})
+// app.use('/api/restaurant/:rid', (req, res) => {
+//   overview.get(`/api/restaurant/${req.params.id}`)
+//     .then(response => res.json(response.data))
+//     .catch(err => res.status(400).send(err));
+// })
 
 // app.use('/api/restaurant/:id', (req, res) => {
 //   reservations.get(`/api/restaurant/${req.params.id}`)
@@ -38,22 +37,24 @@ app.use('/api/restaurant/:rid', (req, res) => {
 // })
 
 app.use('/api/menu/:id', (req, res) => {
-  menu.get(`/api/menu/${req.params.id}`)
-    .then(response => res.json(response.data))
+  menu.get(`/menu/${req.params.id}`)
+    .then(response => {
+      res.json(response.data);
+    })
     .catch(err => res.status(400).send(err));
-})
+});
 
-app.use('/api/restaurants/:id/reviews', (req, res) => {
-  reviews.get(`/api/restaurants/${req.params.id}/reviews`)
-    .then(response => res.json(response.data))
-    .catch(err => res.status(400).send(err));
-})
+// app.use('/api/restaurants/:id/reviews', (req, res) => {
+//   reviews.get(`/api/restaurants/${req.params.id}/reviews`)
+//     .then(response => res.json(response.data))
+//     .catch(err => res.status(400).send(err));
+// })
 
-app.use('/api/restaurants/:id/filters', (req, res) => {
-  reviews.get(`/api/restaurants/${req.params.id}/filters`)
-    .then(response => res.json(response.data))
-    .catch(err => res.status(400).send(err));
-})
+// app.use('/api/restaurants/:id/filters', (req, res) => {
+//   reviews.get(`/api/restaurants/${req.params.id}/filters`)
+//     .then(response => res.json(response.data))
+//     .catch(err => res.status(400).send(err));
+// })
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
